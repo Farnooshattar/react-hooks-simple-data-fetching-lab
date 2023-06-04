9441449a35e86261c2c98f52d3fdab1176b9a730
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-06-04 02:09:19 -0400

    fetches a random image using sideeffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+export default function App() {
+    const [randImage, setImage]= useState("")
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setImage(data.message);
+            setIsLoaded(true);
+          });
+    }, []);
+    if (!isLoaded) return <h3>Loading...</h3>;
+    return (
+        <p><img src={randImage} alt="A Random Dog"/></p>
+    )
+}
+ 
